c7348044041777eb571ac8e794af15a145c2b3c7
Fábio Silva <60890021+linkineo2002@users.noreply.github.com>
2022-10-05 16:50:17 -0300

    Create Main.java
    Desafio de código 3 - Uma Chamada Recursiva

---
+++ Desafio-Uma-Chamada-Recursiva/Main.java
@@ -0,0 +1,42 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.
+
+//Descrição: Neste desafio, receba um número inteiro N,
+//calcule e imprima o somatório de todos os números de N até 0.
+
+//Entrada: A Entrada será composta por um número inteiro, N.
+
+//Saída: Será  impresso o somatório de N até 0, como no exemplo a baixo:
+//Entrada   |   Saída
+//  10      |   55
+//  4       |   10
+//  15      |   120
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner numero = new Scanner(System.in);
+        int n;
+
+        System.out.println("Escolha um valor para N: ");;
+        n = numero.nextInt();
+        System.out.println(somatorio(n));
+
+        //TODO: Imprima o somatório de N (utilize o método "somatorio").
+    }
+
+    /**
+     * Método útil que calcula o somatório de um número usando recursividade.
+     */
+    static int somatorio(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else {
+            return numero + somatorio(numero - 1);
+        }
+    }
+}
